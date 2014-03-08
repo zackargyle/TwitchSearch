c17d1b870cacce473a95db32d1c73ea0c50b13d7
Zack Argyle <zargyle@verisage.us>
2014-03-08 12:58:19 -0700

    Aesthetic fixes

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ Twitch.updateList = (function() {
   function updatePageAvailability() {
   	var page = Twitch.pageCount > 0 ? Twitch.data_offset + 1 : 0;
   	pageIndex.innerHTML = '&nbsp ? &nbsp'.replace('?', page + '/' + Twitch.pageCount);
-		if ((Twitch.data_offset) > Twitch.pageCount) {
+		if (Twitch.data_offset >= Twitch.pageCount - 1) {
 			pageNext.className = 'disabled';
 			pageNext.onclick   = null;
 		} else {
@@ -143,3 +143,17 @@ window.twitchCallback = (function() {
 		}
 	};
 }());
+
+// Initialize search
+(function() {
+  var val = document.getElementById('twitch-input');
+  if (val) Twitch.search();
+}());
+
+window.onresize = (function() {
+	var elem = document.getElementById('twitch');
+  return function() {
+  	elem.style.marginLeft = (window.innerWidth - elem.offsetWidth) / 2 + 'px';
+  }
+}());
+window.onresize();
