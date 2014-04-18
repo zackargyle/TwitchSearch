ac1b86a5dea4bd5400dcd98d6eee45c10472644e
Zack Argyle <zargyle@verisage.us>
2014-04-18 07:54:28 -0600

    Overhaul

---
+++ js/HTMLArray.js
@@ -0,0 +1,289 @@
+'use strict';
+
+/*
+
+** Array ***
+
+set
+push
+splice
+slice
+concat
+move
+clear
+get
+getAll
+
+*** PAGINATION ***
+
+initPagination
+setPageIndex
+setPageSize
+nextPage
+prevPage
+isLastPage
+isFirstPage
+getPageIndex
+
+*/
+
+function HTMLArray (id, data) {
+
+	var element = document.getElementById(id),
+			parent  = element.parentNode,
+			display = element.style.display,
+			pageIndex, pageSize,
+			firstPage, lastPage,
+			paginationEnabled = false,
+			nodes  = [], this_ = this;
+
+	element.style.display = "none";
+
+	function getValue(obj, attrs) {
+		for (var i = 0; i < attrs.length; i++) {
+			obj = obj[attrs[i]];
+		}
+		return obj;
+	}
+
+	function setupChild(node, obj) {
+		if (node.getAttribute("x-value")) {
+			var attr = node.getAttribute("x-value").split(".");
+			if (node.nodeName === "INPUT" && node.type === "text") {
+				node.value = getValue(obj, attr);
+			} else {
+				node.innerHTML = getValue(obj, attr) + node.innerHTML;
+			}
+		}
+		if (node.getAttribute("x-class")) {
+			var attr = node.getAttribute("x-value").split(".");
+			node.className += getValue(obj, attr).toLowerCase();
+		}
+		if (node.getAttribute("x-href")) {
+			var attr = node.getAttribute("x-href").split(".");
+			node.href = getValue(obj, attr);
+		}
+		if (node.getAttribute("x-src")) {
+			var attr = node.getAttribute("x-src").split(".");
+			node.src = getValue(obj, attr);
+		}
+		for (var i = 0; i < node.children.length; i++) {
+			setupChild(node.children[i], obj);
+		}
+	}
+
+	function refresh() {
+		var start = 0, end = nodes.length;
+
+		if (paginationEnabled) {
+			start = pageIndex * pageSize;
+			end = start + pageSize;
+		}
+
+		for (var i = 0; i < nodes.length; i++) {
+			if (i >= start && i < end) {
+				nodes[i].style.display = display;
+			} else {
+				nodes[i].style.display = "none";
+			}
+		}
+	}
+
+	function insertNode(obj, index) {
+		if (index === undefined) 
+			index = data.length;
+
+		var clone = element.cloneNode(true);
+		clone.removeAttribute("id");
+		setupChild(clone, obj);
+
+		parent.insertBefore(clone, nodes[index]);
+		nodes.splice(index, 0, clone);
+		data.splice(index, 0, obj);
+	}
+
+	function remove(index) {
+		parent.removeChild(nodes[index]);
+		nodes.splice(index,1);
+		data.splice(index,1);
+	}
+
+	function removeAll() {
+		for (var i = 0; i < nodes.length; i++)
+			parent.removeChild(nodes[i]);
+		nodes = [];
+		data = [];
+	}
+
+	function isArray(obj) {
+		return Object.prototype.toString.call(obj) === '[object Array]';
+	}
+
+	this.set = function(updatedData) {
+		if (!isArray(updatedData)) {
+			throw "HTMLArray.set requires an array";
+		}
+
+		removeAll();
+
+		for (var i = 0; i < updatedData.length; i++) {
+			insertNode(updatedData[i]);
+		}
+		refresh();
+		return this_;
+	}
+
+	this.push = function(obj) {
+		if (!obj) throw "HTMLArray.push requires an object.";
+		insertNode(obj);
+		refresh();
+		return this_;
+	}
+
+	this.splice = function(index, num, obj) {
+		index = parseInt(index), num = parseInt(num);
+		if (isNaN(index) || isNaN(num)) {
+			throw "HTMLArray.splice requires 2 paramters: index, num.";
+		}
+		while (--num >= 0) {
+			remove(index);
+		}
+		
+		if (obj) {
+			insertNode(obj, index);
+		}
+
+		refresh();
+		return this_;
+	}
+
+	this.concat = function(array) {
+		if (!isArray(array)) {
+			throw "HTMLArray.concat requires an array";
+		}
+
+		for (var i = 0; i < array.length; i++) {
+			insertNode(array[i]);
+		}
+
+		refresh();
+		return this_;
+	}
+
+	this.move = function(from, to) {
+		from = parseInt(from), to = parseInt(to);
+		if (isNaN(from) || isNaN(to)) {
+			throw "HTMLArray.move requires 2 indexes: from, to.";
+		}
+
+		var obj = data[from],
+				node = nodes[from];
+
+		parent.removeChild(node);
+		parent.insertBefore(node, nodes[to]);
+		nodes.splice(to, 0, nodes.splice(from, 1)[0]);
+		data.splice(to, 0, data.splice(from, 1)[0]);
+
+		refresh();
+		return this_;
+	}
+
+	this.slice = function(begin, end) {
+		begin = parseInt(begin), end = parseInt(end);
+		if (isNaN(begin) || isNaN(end)) {
+			throw "HTMLArray.slice requires 2 indexes: begin, end.";
+		}
+		return this.set(data.slice(begin, end));
+	}
+
+	this.clear = function() {
+		if (paginationEnabled) {
+			pageIndex = 0;
+		}
+		return this.set([]);
+	}
+
+	this.get = function(index) {
+		if (index === undefined) {
+			throw "HTMLArray.get requires an index";
+		}
+		return data[index];
+	}
+
+	this.getAll = function() {
+		return data;
+	}
+
+	/* PAGINATION */
+	function showCurrentPage(makeVisible) {
+		var	start = pageIndex * pageSize,
+				end   = start + pageSize;
+		if (end > nodes.length) 
+			end = nodes.length;
+		for (var i = start; i < end; i++) 
+			nodes[i].style.display = makeVisible ? display : "none";
+	}
+
+	this.initPagination = function(pageSize_, pageIndex_, refresh_) {
+		paginationEnabled = true;
+		pageIndex = pageIndex_ || 0;
+		pageSize = pageSize_ || 10;
+
+		if (refresh_) refresh();
+		return this_;
+	}
+
+	this.setPageIndex = function(pageIndex_) {
+		if (pageIndex !== pageIndex_) {
+			showCurrentPage(false);
+			pageIndex = pageIndex_
+			showCurrentPage(true);
+		}
+		return this_;
+	}
+
+	this.setPageSize = function(pageSize_) {
+		if (pageSize !== pageSize_) {
+			if (pageSize > pageSize_) {
+				showCurrentPage(false);
+			}
+			pageSize = pageSize_;
+			showCurrentPage(true);
+		}
+	}
+
+	this.nextPage = function() {
+		if (!this.isLastPage()) {
+			showCurrentPage(false);
+			pageIndex++;
+			showCurrentPage(true);
+		}
+		return this_;
+	}
+
+	this.prevPage = function() {
+		if (!this.isFirstPage()) {
+			showCurrentPage(false);
+			pageIndex--;
+			showCurrentPage(true);
+		}
+		return this_;
+	}
+
+	this.isLastPage = function() {
+		if (data.length > 0)
+			return pageIndex === Math.ceil(data.length / pageSize) - 1;
+		else
+			return true;
+	}
+
+	this.isFirstPage = function() {
+		return pageIndex === 0;
+	}
+
+	this.getPageIndex = function() {
+		return pageIndex + 1;
+	}
+
+	this.set(data || []);
+}
--- js/app.js
+++ js/app.js
@@ -1,200 +1,67 @@
-'use strict';
 
-/*
-  Object to handle Twitch API consumption
-*/
-var Twitch = {
-	requestCount: 0,
-	pageCount: 0,
-	data_set: null,
-	data_set_next: null,
-	data_set_prev: null,
-	data_offset: 0,
-	debounce: null
-};
-
-// Display next page of data
-Twitch.nextPage = function() {
-	this.data_offset++;
-	this.data_set_prev = this.data_set;
-	this.data_set = this.data_set_next;
-
-	this.updateList();
-	this.queue.next();
-}
-
-// Display previous page of data
-Twitch.prevPage = function() {
-	this.data_offset--;
-	this.data_set_next = this.data_set;
-	this.data_set = this.data_set_prev;
-
-	this.updateList();
-	this.queue.prev();
-}
-
-/*
-  For efficiency, queue up prev/next page of data
-*/
-Twitch.queue = (function() {
-	var next_url   = 'https://api.twitch.tv/kraken/search/streams?limit=5&callback=queueNext&q=',
-	    prev_url   = 'https://api.twitch.tv/kraken/search/streams?limit=5&callback=queuePrev&q=',
-	    searchBar  = document.getElementById('twitch-input');
-
-	return {
-		next: function() {
-			var val    = searchBar.value,
-			    offset = Twitch.data_offset * 5 + 5,
-			    request_url = next_url + val + '&offset=' + offset;
-			Twitch.get(request_url);
-		},
-		prev: function() {
-			var val = searchBar.value,
-			    offset = Twitch.data_offset * 5 - 5,
-			    request_url = prev_url + val + '&offset=' + offset;
-			Twitch.get(request_url);
-		}
-	}
-}());
-
-/*
-  Method for updating current list of displayed streams
-    - Should update pagination arrows
-    - Should build info and display all at once
-*/
-Twitch.updateList = (function() {
-
-	var list  = document.getElementById('twitch-list'),
-	    pageIndex = document.getElementById('page-index'),
-	    pagePrev  = document.getElementById('page-prev'),
-	    pageNext  = document.getElementById('page-next'),
-	    streamTotal = document.getElementById('streamTotal');
-
-	// Helper function to update pagination arrows
-  function updatePageAvailability() {
-  	var page = Twitch.pageCount > 0 ? Twitch.data_offset + 1 : 0;
-  	pageIndex.innerHTML = '&nbsp ? &nbsp'.replace('?', page + '/' + Twitch.pageCount);
-
-		if (Twitch.data_offset >= Twitch.pageCount - 1) {
-			pageNext.className = 'disabled';
-			pageNext.onclick   = null;
-			pageNext.touchend  = null;
-		} else {
-			pageNext.className = '';
-			pageNext.onclick   = function() {Twitch.nextPage()};
-			pageNext.touchend  = function() {Twitch.nextPage()};
-		}
-
-		if (Twitch.data_offset === 0) {
-			pagePrev.className = 'disabled';
-			pagePrev.onclick   = null;
-			pagePrev.touchend  = null;
-		} else {
-			pagePrev.className = '';
-			pagePrev.onclick   = function() {Twitch.prevPage()};
-			pagePrev.touchend  = function() {Twitch.prevPage()};
-		}
-	}
-
-	function createStreamString(stream) {
-		var html = "<div class='twitch-stream'>";
-		   html += "  <a href='#'><img class='stream-image' src='?'></img></a>".replace('?', stream.preview.medium).replace('#', stream.channel.url);
-		   html += "  <div class='stream-info'>";
-		   html += "    <a class='stream-name' href='#'> ?</a>".replace('?', stream.channel.display_name).replace('#', stream.channel.url);
-		   html += "    <div class='stream-game'> ?</div>".replace('?', stream.game + ' - ' + stream.viewers + ' viewers');
-		   html += "    <div class='stream-desc'> ?</div>".replace('?', stream.channel.status);
-		   html += "</div></div>";
-		return html;
-	}
-
-	return function() {
-		this.pageCount = Math.ceil(this.data_set._total / 5);
-		streamTotal.innerHTML = this.data_set._total;
-
-		var html = '';
-		this.data_set.streams.forEach(function(stream) {
-			html += createStreamString(stream);
-		});
-		updatePageAvailability();
-		list.innerHTML = html;
-		list.style.display = 'block';
-	};
-
-}());
-
-/*
-  Method for searching for twitch streams matching input query.
-	  - Should be debounced for efficiency
-	  - Should limit results to 5
-*/
-Twitch.search = (function() {
-	var url   = 'https://api.twitch.tv/kraken/search/streams?limit=5&callback=search&q=',
-	    list  = document.getElementById('twitch-list'),
-	    searchBar  = document.getElementById('twitch-input'),
-	    prevVal = '';
-
-	return function() {
-		clearTimeout(this.debounce);
-		var val = searchBar.value;
-
-		// Submitted from keydown (not pagination)
-		this.data_offset = 0;
-
-		// Twitch only responds with data for searches of length > 2
-		if (val.length > 2) {
-			var request_url = url + val + '&offset=' + Twitch.data_offset * 5;
-
-			this.debounce = setTimeout(function() {
-				Twitch.get(request_url);
-			}, 200);
-
-		}
-	};
-}());
-
-// Base cross domain request
-Twitch.get = function(url) {
-	var script = document.createElement('script');
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
-	this.requestCount++;
-}
-
-/* 
-  Method to handle Twitch API response
-    - Should check for correct data
-    - Should only update view if no other requests are out
-*/
-window.search = function(response) {
-	Twitch.requestCount--;
-	// Only change view if another request has not been sent
-	if (response.streams && Twitch.requestCount === 0) {
-		Twitch.data_set = response;
-		Twitch.updateList();
-		Twitch.queue.next();
-	}
-};
-
-window.queueNext = function(response) {
-	if (response && response.streams)
-		Twitch.data_set_next = response;
-};
-
-window.queuePrev = function(response) {
-	if (response && response.streams)
-		Twitch.data_set_prev = response;
-};
 
 // Initialize search
 (function() {
-	var val = document.getElementById('twitch-input');
-	if (val) Twitch.search();
+	var	url    = 'https://api.twitch.tv/kraken/search/streams?limit=15&callback=',
+	    list   = document.getElementById('twitch-list'),
+	    input = document.getElementById('twitch-input'),
+	    total = document.getElementById('streamTotal'),
+	    offset = 0, debounce = null, height = 0, prevValue;
+
+	var twitch   = new HTMLArray("stream-node")
+	
+	document.getElementById('twitch-input').onkeyup = search;
+	document.getElementById('twitch-submit').onclick = search;
+	document.getElementById('twitch-submit').touchend = search;
+	window.onscroll = scroll;
+
+	window.response = function(response) {
+		if (response.streams) {
+			if (offset === 0) {
+				total.innerHTML = response._total;
+				twitch.set(response.streams);
+			} else {
+				twitch.concat(response.streams);
+			}
+		}
+	};
+
+	function scroll() {
+		if (window.scrollY === document.documentElement.clientHeight - window.innerHeight) {
+			if (window.scrollY !== height) {
+				height = window.scrollY;
+				var value = input.value.trim();
+				offset += 15;
+				get(url + 'response&q=' + value + '&offset=' + offset);
+			}
+		}
+	}
+
+  function get(url) {
+	 	var script = document.createElement('script');
+		script.src = url;
+		document.getElementsByTagName('head')[0].appendChild(script);
+  }
+
+	function search() {
+		var value = input.value.trim();
+		if (value !== prevValue) {
+			prevValue = value;
+			clearTimeout(debounce);
+			debounce = setTimeout(function() {
+				offset = 0;
+				get(url + 'response&q=' + value + '&offset=' + offset);
+			}, 200);
+		}
+	}
+	
 }());
 
 window.onresize = (function() {
 	var elem = document.getElementById('twitch');
 	return function() {
-		elem.style.marginLeft = (window.innerWidth - elem.offsetWidth) / 2 + 'px';
+		if (window.innerWidth > 700)
+			elem.style.marginLeft = (window.innerWidth - elem.offsetWidth) / 2 + 'px';
 	}
 }());
 window.onresize();
